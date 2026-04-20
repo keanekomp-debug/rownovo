5fe8b5c568e940fe7f75cc13201b00ea0d307628
keanekomp-debug <keanekomp@gmail.com>
2026-04-20 20:06:46 +0100

    Refactor getBuoys to use imported data

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,5 @@
-import buoys from "@/data/buoys.json";
+import data from '@/data/buoys.json';
 
-export async function getBuoys() {
-  // TODO: Replace DEMO_BUOYS with live GPS feed
-  // or connect to your Supabase/REST endpoint here
-  return buoys.buoys;
+export function getBuoys() {
+  return data.buoys;
 }
