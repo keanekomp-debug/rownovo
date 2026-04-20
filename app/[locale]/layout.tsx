c7b0d2b199ce16ab9681be87d5f92614c96644a3
keanekomp-debug <keanekomp@gmail.com>
2026-04-20 14:17:23 +0100

    Add LocaleLayout component with internationalization

---
+++ app/[locale]/layout.tsx
@@ -0,0 +1,20 @@
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
+import Navbar from "@/components/layout/Navbar";
+import Footer from "@/components/layout/Footer";
+
+export default async function LocaleLayout({ children, params }: any) {
+  const messages = await getMessages();
+
+  return (
+    <html lang={params.locale}>
+      <body>
+        <NextIntlClientProvider messages={messages}>
+          <Navbar />
+          <main>{children}</main>
+          <Footer />
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
