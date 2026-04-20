1481908e8700142520f2802a1ac48bbd309b24c5
keanekomp-debug <keanekomp@gmail.com>
2026-04-20 20:07:13 +0100

    Refactor BuoyMap to fetch and display buoy data
    Refactor BuoyMap component to manage buoy state and display markers on the map.

--- components/tracker/BuoyMap.tsx
+++ components/tracker/BuoyMap.tsx
@@ -1,11 +1,58 @@
-"use client";
+'use client';
 
-import dynamic from "next/dynamic";
+import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import {useEffect, useState} from 'react';
+import {getBuoys} from '@/lib/api';
 
-const MapCore = dynamic(() => import("./MapCore"), {
-  ssr: false
+const defaultIcon = L.icon({
+  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41]
 });
 
-export default function BuoyMap({ buoys, onSelect }: any) {
-  return <MapCore buoys={buoys} onSelect={onSelect} />;
+export default function BuoyMap() {
+  const [buoys, setBuoys] = useState<any[]>([]);
+
+  useEffect(() => {
+    setBuoys(getBuoys());
+  }, []);
+
+  return (
+    <MapContainer
+      center={[39.5, -8.0]}
+      zoom={7}
+      style={{height: '100vh', width: '100%'}}
+    >
+      <TileLayer
+        attribution='&copy; OpenStreetMap contributors'
+        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+      />
+
+      {buoys.map((buoy) => (
+        <Marker
+          key={buoy.id}
+          position={[buoy.lat, buoy.lng]}
+          icon={defaultIcon}
+        >
+          <Popup>
+            <strong>{buoy.name}</strong>
+            <br />
+            Status: {buoy.status}
+            <br />
+            pH: {buoy.readings.ph}
+            <br />
+            Temp: {buoy.readings.temperature}°C
+            <br />
+            DO: {buoy.readings.dissolvedOxygen}
+            <br />
+            Conductivity: {buoy.readings.conductivity}
+            <br />
+            Turbidity: {buoy.readings.turbidity}
+          </Popup>
+        </Marker>
+      ))}
+    </MapContainer>
+  );
 }
