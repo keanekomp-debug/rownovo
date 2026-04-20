dd4092a9cea82bd4fc931324f48b0ab875e47087
keanekomp-debug <keanekomp@gmail.com>
2026-04-20 19:38:59 +0100

    Integrate next-intl plugin into Next.js config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+const withNextIntl = require('next-intl/plugin')();
 
-module.exports = nextConfig;
+module.exports = withNextIntl({
+  // your existing config (if any)
+});
