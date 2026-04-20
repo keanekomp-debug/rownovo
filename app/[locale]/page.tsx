757049e7f823038dca8c86fd56c77fa305eb9053
keanekomp-debug <keanekomp@gmail.com>
2026-04-20 15:00:30 +0100

    Add WaveDivider to HomePage component
    Refactor HomePage component to include WaveDivider.

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -3,18 +3,6 @@ import StatsStrip from "@/components/home/StatsStrip";
 import MissionSection from "@/components/home/MissionSection";
 import HowItWorksSection from "@/components/home/HowItWorksSection";
 import CTASection from "@/components/home/CTASection";
-
-export default function HomePage() {
-  return (
-    <>
-      <HeroSection />
-      <StatsStrip />
-      <MissionSection />
-      <HowItWorksSection />
-      <CTASection />
-    </>
-  );
-}
 import WaveDivider from "@/components/ui/WaveDivider";
 
 export default function HomePage() {
