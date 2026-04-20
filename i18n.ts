25b5c61c7047a2c604d7cf111bcc4d470d364ad6
keanekomp-debug <keanekomp@gmail.com>
2026-04-20 20:00:37 +0100

    Add Portuguese locale to i18n configuration

--- i18n.ts
+++ i18n.ts
@@ -1,7 +1,7 @@
 import {notFound} from 'next/navigation';
 import {getRequestConfig} from 'next-intl/server';
 
-export const locales = ['en'];
+export const locales = ['en', 'pt'];
 
 export default getRequestConfig(async ({locale}) => {
   if (!locales.includes(locale as any)) notFound();
