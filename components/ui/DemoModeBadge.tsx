4c3b220aba5cb0ffbb9149524325a69422b23b9e
keanekomp-debug <keanekomp@gmail.com>
2026-04-20 14:25:30 +0100

    Add DemoModeBadge component for prototype indication

---
+++ components/ui/DemoModeBadge.tsx
@@ -0,0 +1,7 @@
+export default function DemoModeBadge() {
+  return (
+    <div className="absolute top-4 right-4 bg-alert text-black px-3 py-1 text-xs rounded">
+      PROTOTYPE MODE
+    </div>
+  );
+}
