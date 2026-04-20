1f4d0d2e96eed329dd6af4fcf67622fc1b2f3d57
keanekomp-debug <keanekomp@gmail.com>
2026-04-20 20:11:10 +0100

    Refactor TrackerPage to use dynamic BuoyMap import
    Refactor TrackerPage component to use dynamic import for BuoyMap and simplify layout.

--- app/[locale]/tracker/page.tsx
+++ app/[locale]/tracker/page.tsx
@@ -1,33 +1,14 @@
-"use client";
+import dynamic from 'next/dynamic';
 
-import dynamic from "next/dynamic";
-import { useEffect, useState } from "react";
-import { getBuoys } from "@/lib/api";
-import BuoySidebar from "@/components/tracker/BuoySidebar";
-import DemoModeBadge from "@/components/ui/DemoModeBadge";
-
-const BuoyMap = dynamic(() => import("@/components/tracker/BuoyMap"), {
-  ssr: false
-});
+const BuoyMap = dynamic(
+  () => import('@/components/tracker/BuoyMap'),
+  {ssr: false}
+);
 
 export default function TrackerPage() {
-  const [buoys, setBuoys] = useState<any[]>([]);
-  const [selected, setSelected] = useState<any>(null);
-
-  useEffect(() => {
-    getBuoys().then(setBuoys);
-  }, []);
-
   return (
-    <div className="h-[calc(100vh-64px)] flex">
-      <div className="flex-1 relative">
-        <BuoyMap buoys={buoys} onSelect={setSelected} />
-        <DemoModeBadge />
-      </div>
-
-      <div className="w-[400px]">
-        <BuoySidebar buoy={selected} />
-      </div>
-    </div>
+    <main style={{height: '100vh'}}>
+      <BuoyMap />
+    </main>
   );
 }
